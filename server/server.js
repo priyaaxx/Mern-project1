0ed966e5028f5add3b594a3be654ca99d7e2bb55
priyaaxx <priyagopalsmc@gmail.com>
2024-08-03 12:40:30 +0530

    new commit

--- server/server.js
+++ server/server.js
@@ -13,6 +13,18 @@ app.use('/api/users', usersRoute);
 app.use('/api/products', productsRoute);
 app.use('/api/bids', bidsRoute);
 
+// deployment config
+const path = require("path");
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+
 app.listen(port, ()=>{
     console.log(`Node/Express Server started on port ${port}`)
 });
